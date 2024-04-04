84d2cceb352bf935ae6d1aab6917c9f597844c59
Collpredartifact <collpredartifact@gmail.com>
2024-04-04 15:06:21 -0700

    bash scripts for the figure

---
+++ motion_planning_prediction/fig15.sh
@@ -0,0 +1,19 @@
+bash launch_bit.sh
+
+python plot_fig15.py mpnet_7d
+echo "Reproduced Figure 15: open mpnet_7d.pdf"
+
+python plot_fig15.py mpnet_2d
+echo "Reproduced Figure 15: open mpnet_2d.pdf"
+
+python plot_fig15.py gnn_7d
+echo "Reproduced Figure 15: open gnn_7d.pdf"
+
+python plot_fig15.py gnn_2d
+echo "Reproduced Figure 15: open gnn_2d.pdf"
+
+python plot_fig15.py bit_7d
+echo "Reproduced Figure 15: open bit_7d.pdf"
+
+python plot_fig15.py bit_2d
+echo "Reproduced Figure 15: open bit_2d.pdf"
---
+++ motion_planning_prediction/fig16.sh
@@ -0,0 +1,4 @@
+bash launch_perf.sh
+
+python plot_fig16.py
+echo "Reproduced Figure 16: open perf_area_plot.pdf"
