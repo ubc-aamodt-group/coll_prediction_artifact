e35f90adf7a19bc1d56b3d6cc5acbe4327e26f72
Collpredartifact <collpredartifact@gmail.com>
2024-04-10 15:30:56 -0700

    fixed the filename

--- motion_planning_prediction/launch_bit.sh
+++ motion_planning_prediction/launch_bit.sh
@@ -3,17 +3,17 @@ python prediction_simulation_2D.py 1 1 64 MPNET | tee logfile result_files/mpnet
 python prediction_simulation_2D.py 1 1 64 BIT | tee logfile result_files/bit_2d_pred.csv 
 python prediction_simulation_2D.py 1 1 64 GNN | tee logfile result_files/gnn_2d_pred.csv 
 
-python csp_simulation_2D.py MPNET | tee logfile result_files/mpnet_2d_csp.csv 
-python csp_simulation_2D.py BIT | tee logfile result_files/bit_2d_csp.csv 
-python csp_simulation_2D.py GNN | tee logfile result_files/gnn_2d_csp.csv 
+python CSP_simulation_2D.py MPNET | tee logfile result_files/mpnet_2d_csp.csv 
+python CSP_simulation_2D.py BIT | tee logfile result_files/bit_2d_csp.csv 
+python CSP_simulation_2D.py GNN | tee logfile result_files/gnn_2d_csp.csv 
 
 python prediction_simulation_nDOF.py 1 0.125 8 MPNET | tee logfile result_files/mpnet_nDOF_pred.csv 
 python prediction_simulation_nDOF.py 1 0.125 8 BIT | tee logfile result_files/bit_nDOF_pred.csv 
 python prediction_simulation_nDOF.py 1 0.125 8 GNN | tee logfile result_files/gnn_nDOF_pred.csv 
 
-python csp_simulation_nDOF.py MPNET | tee logfile result_files/mpnet_nDOF_csp.csv 
-python csp_simulation_nDOF.py BIT | tee logfile result_files/bit_nDOF_csp.csv 
-python csp_simulation_nDOF.py GNN | tee logfile result_files/gnn_nDOF_csp.csv 
+python CSP_simulation_nDOF.py MPNET | tee logfile result_files/mpnet_nDOF_csp.csv 
+python CSP_simulation_nDOF.py BIT | tee logfile result_files/bit_nDOF_csp.csv 
+python CSP_simulation_nDOF.py GNN | tee logfile result_files/gnn_nDOF_csp.csv 
 
 cd result_files
 paste -d " " gnn_2d_csp.csv gnn_2d_pred.csv > gnn_2d.csv
